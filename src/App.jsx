28e0de3136c5e00a1fd2a8a6ebe3d70b261419fe
Omni <coven3d2015@hotmail.com>
2023-01-27 23:53:50 -0700

    tabs :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
-import { Tab, TabList, TabPanel, useTabState } from "ariakit/tab";
+import { Tab, TabList, TabPanel, useTab, useTabState } from "ariakit/tab";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import TestComp from "./components/TestComp";
+import Desk from "./components/Desk";
+import "./stylesheets/tabBar.css"
 
 function App() {
+  const defaultTabID = "default-selected-tab";
+  const tab = useTabState({defaultTabID})
   return (
     <div className="container">
-      <TestComp passingState={3} />
-      <h1>test</h1>
-      <h1>hi omner</h1>
+      <h3>Sweet freaking webapp :D</h3>
+      <TabList state={tab} className="outer-tabs-container" aria-label="tabs">
+        <Tab className="outer-tab" id={defaultTabID}>Desk</Tab>
+        <Tab className="outer-tab">TestComp</Tab>
+      </TabList>
+      <div className="panels">
+        <TabPanel state={tab} tabId={defaultTabID}>
+          <Desk/>
+        </TabPanel>
+        <TabPanel state={tab}>
+          <TestComp/>
+        </TabPanel>
+      </div>
     </div>
   );
 }
---
+++ src/components/Desk.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Desk() {
+  return (
+	<div>
+	  <h1>Hello this is the desk component</h1>
+	  <h2>This component should be the default when the application starts</h2>
+	</div>
+  )
+}
